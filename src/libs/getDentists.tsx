b4a9265142608a28d071028dcb4a485536587c7b
Pattarapon Noonun <fluk3874@gmail.com>
2025-03-24 11:03:52 +0700

    add getDentist and update Booking(not yet)

---
+++ src/libs/getDentists.tsx
@@ -0,0 +1,22 @@
+export default async function getDentists( token: string ) {
+    try {
+        const response = await fetch("http://localhost:5000/api/v1/dentists", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message || "Failed to fetch dentists");
+        }
+
+        return data;
+    } 
+    catch (error: any) {
+        console.error("Dentists fetch error:", error.message || error);
+        return null;
+    }
+};
