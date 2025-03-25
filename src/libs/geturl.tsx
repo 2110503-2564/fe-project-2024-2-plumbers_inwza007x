9dfc1712c3ca958630ddbbdb29a48118b2b02494
opalInwza007x <opallortisood@gmail.com>
2025-03-25 17:42:14 +0700

    Change API Link

--- src/libs/geturl.tsx
+++ src/libs/geturl.tsx
@@ -1,3 +1,3 @@
-const BACKEND_URL="https://lnwza696969.us-east-1.elasticbeanstalk.com";
+const BACKEND_URL="https://backend-pj-for-my-frontend-pj-f9fy.vercel.app";
 // const BACKEND_URL = "http://localhost:5000"
 export default BACKEND_URL;
